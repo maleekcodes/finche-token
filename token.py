8e02db3f1c7e55dcfca57c141eee890bb9c644b3
maleekcodes <abduljrfreshy@gmail.com>
2018-07-20 02:38:34 +0000

    Working Version, more commits coming to scale down the program

--- token.py
+++ token.py
@@ -1,11 +1,9 @@
 from stellar_base.keypair import Keypair
 from stellar_base.asset import Asset
-from stellar_base.transaction import Transaction
-from stellar_base.transaction_envelope import TransactionEnvelope
-from stellar_base.operation import Payment, ChangeTrust
-from stellar_base.address import Address
+from stellar_base.builder import Builder
 from stellar_base.horizon import horizon_testnet
-from stellar_base.memo import TextMemo
+from stellar_base.address import Address
+
 import requests
 import base64
 
@@ -24,95 +22,51 @@ if __name__ == '__main__':
     # address, password = gen_address()
     # print(address, password)
 
-    ISSUER_ADDRESS  = 'GB6ECAPN2AWTJ3OI5RQD3OWOOS3UBNL5CGK4EKQRKKLIH5V2MJKZAT44'
-    HOLDER_ADDRESS  = 'GBJ3GXL2LRW5JTVEJFER2M2VHGFUY4WJGCEXBQIDVVVE4QC2PLJYV72U'
-    ISSUER_SEED     = 'SBUVNJJ4EMIJHEPPVQVYSNDS3M3KAYUFYUVSRKRN3YN7WRTMJDMIWTK7'
-    HOLDER_SEED     = 'SCZCRMN42FTE3X3OHB65C3Y5U4DASGY43V75GRV6ICMPI3M5R6MD4NEC'
+    ISSUERS_ADDRESS =   "GCA3IT4NDEAZN5QPPWB72D2X5HFT3GPNONJKCAYHCBHN7MGBSYF4VEFU"
+    ISSUERS_SEED    =   "SBFYTBQQPSIP3CSWU43FDFRPMETW56O7J2RUKVOKLDFL6XC4OTVUAUIT"
 
-    #We use this to fund our wallet with some Lumens on the testnet
-    # cash = fund_account(HOLDER_ADDRESS)
-    # print(cash)
+    RECEIVING_ADDRESS   =   "GBYXARVIA3SF5PQZOOKX2RRPZNUDWCERQZ7HEY5GQ4AZ32LD7M3J6RV2"
+    RECEIVING_SEED      =   "SAJDKZPCNBYCGVRH7C3M5CJ2YAXVFCKLFJKID7S2VONJIVYDNUPCRHIU"
 
-    horizon = horizon_testnet()
+    horizon     =   horizon_testnet()
 
-    issuer  =   Keypair.from_seed(ISSUER_SEED)
-    holder  =   Keypair.from_seed(HOLDER_SEED)
-
-    #Now let's get the account information
-
-
-    iss     =   Address(address=issuer.address().decode())
-    hodl    =   Address(address=holder.address().decode())
-
-    # print(issuer.address().decode())
-
-    iss.get()
-    hodl.get()
-
-    # print("=" * 50)
-    # print("=" *50)
-    # print("ISSUER DETAILS")
-    # print("=" * 50)
-    # print("=" *50)    
+    ISSUER        =   Address(address=ISSUERS_ADDRESS)
+    RECEIVER        =   Address(address=RECEIVING_ADDRESS)
+    ISSUER.get()
+    RECEIVER.get()
     
-    # print("Balances: {}".format(iss.balances))
-    # print("Sequence Number: {}".format(iss.sequence))
-    # print("Flags: {}".format(iss.flags))
-    # print("Signers: {}".format(iss.signers))
-    # print("Data: {}".format(iss.data))
+    #We get the balance of ISSUER and RECEIVER
 
-    # print("\n")
-    # print("\n")
+    ISSUER_BALANCE      =   ISSUER.balances
+    RECEIVER_BALANCE    =   RECEIVER.balances
 
-    # print("=" * 50)
-    # print("=" *50)
-    # print("HOLDER DETAILS")
-    # print("=" * 50)
-    # print("=" *50)  
+    #We use testnet to fund the account for now
+    # req = fund_account(RECEIVING_ADDRESS) 
+    # print(req)
 
-    # print("Balances: {}".format(hodl.balances))
-    # print("Sequence Number: {}".format(hodl.sequence))
-    # print("Flags: {}".format(hodl.flags))
-    # print("Signers: {}".format(hodl.signers))
-    # print("Data: {}".format(hodl.data))
+    asset = Asset("FT", ISSUERS_ADDRESS)
 
-  
-    asset = Asset('FT', ISSUER_ADDRESS)
-    # create op
-    op = Payment(
-        source=issuer.address().decode(),
-        destination=HOLDER_ADDRESS,
-        asset=asset,
-        amount='100'
+    builder = Builder(
+        RECEIVING_SEED, network="TESTNET"
+    ).append_trust_op(
+        destination=asset.issuer, code = asset.code
     )
-    # create a memo
-    msg = TextMemo('Buy Finche Token!')
 
+    builder.sign()
+    resp = builder.submit()
+    print(resp)
 
-    sequence = horizon.account(holder.address().decode('utf-8')).get('sequence')
-    #Lets trust our own Token
-    # trust       =   ChangeTrust({'asset': asset, 'limit': '1000'})
+    Now we send payment
+    builder = Builder(
+        ISSUERS_SEED, network="TESTNET"
+    ).append_payment_op(
+        destination = RECEIVING_ADDRESS,
+        amount = 1000,
+        asset_code=asset.code,
+        asset_issuer=asset.issuer
 
-    tx = Transaction(
-        source=holder.address().decode(),
-        sequence=sequence,
-        memo = msg,
-        fee=100,
-        operations=[
-            op,
-        ],
     )
-    # Build Envelope
-    envelope    =   TransactionEnvelope(tx=tx, network_id = 'TESTNET')
-    #sign
-    envelope.sign(issuer)
-    #Submit
-    env_xdr = envelope.xdr
-    print(env_xdr)
-    # response =  horizon.submit(base64.b64encode(env_xdr.encode('utf-8')))
-    response =  horizon.submit(env_xdr)
 
-    if 'result_xdr' in response:
-        print("Successfully Transfered")
-    else:
-        print("WTH happened?")
+    builder.sign()
+    resp = builder.submit()
+    print(resp)
